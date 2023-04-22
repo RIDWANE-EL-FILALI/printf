23c887c8f7ba3920fbf7dea1464c536a06da8bb3
RIDWANE-EL-FILALI <ridwaneelfilali@gmail.com>
2023-04-22 16:59:54 -0700

    done with printf

--- ft_putoctal.c
+++ ft_putoctal.c
@@ -7,13 +7,14 @@
  *
  * Rteurn: void
  */
-void    ft_putoctal(unsigned long n, int *count)
+
+void	ft_putoctal(unsigned long n, int *count)
 {
-        if (n >= 8)
-        {
-                ft_putoctal(n / 8, count);
-                ft_putoctal(n % 8, count);
-        }
-        else if (n < 8)
-                ft_putchar(n + 48, count);
+	if (n >= 8)
+	{
+		ft_putoctal(n / 8, count);
+		ft_putoctal(n % 8, count);
+	}
+	else if (n < 8)
+		ft_putchar(n + 48, count);
 }
