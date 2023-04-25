971def5b7dc78fd0497d05d0feff0a28faf8322e
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2023-04-25 21:18:09 +0100

    changes

--- check_width.c
+++ check_width.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
+ * check_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
@@ -10,19 +10,19 @@
  */
 int get_width(const char *format, int *i, va_list list)
 {
-	int curr_i;
+	int indexi;
 	int width = 0;
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (indexi = *i + 1; format[indexi] != '\0'; indexi++)
 	{
-		if (is_digit(format[curr_i]))
+		if (is_digit(format[indexi]))
 		{
 			width *= 10;
-			width += format[curr_i] - '0';
+			width += format[indexi] - '0';
 		}
-		else if (format[curr_i] == '*')
+		else if (format[indexi] == '*')
 		{
-			curr_i++;
+			indexi++;
 			width = va_arg(list, int);
 			break;
 		}
@@ -30,7 +30,7 @@ int get_width(const char *format, int *i, va_list list)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*i = indexi - 1;
 
 	return (width);
 }
